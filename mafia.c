882dbe2c550767aed5b570882c9a8a0b8460a461
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-07-04 20:46:07 +0300

    WIP: Added echo from server on room

--- mafia.c
+++ mafia.c
@@ -2,22 +2,33 @@
 
 // E V E N T S
 
+// EV: Room connections
 /*
-Room connection:
-Connecting:
-	Req: 'rn<name>' - connect to the room with name
-	- Check name<=32 bytes till '\0'
-  - Check if not already connected
-	Res: 'rs' - connection successeful
-On new connection:
-All: 'rl\x1f<name>\x1f<name>...'
-*/
+ * Room connection:
+ * Connecting:
+ *   REQ: 'rc<name>' - connect to the room with name
+ *   - Check name<=32 bytes till '\0'
+ *   - Check if not already connected
+ *   RES: 'rs' - connection successeful
+ *   RES: 're<error_id>' - connection unsuccesseful
+ *   Error ID:
+ *   - 'e' - empty command
+ *   - 'E' - empty name
+ *   - 'l' - name too long
+ * On new connection:
+ *   ALL: 'rl<name>\x1f<name>...'
+ */
+
+void ev_user_room_conn(struct mg_connection* c, char* msg) {
+	mg_ws_send(c, msg, strlen(msg), WEBSOCKET_OP_TEXT);
+}
+
+// EV: High level handlers
 
 void ev_handle_http_msg(struct mg_connection* c, void* ev_data) {
 	struct mg_http_message* hm = (struct mg_http_message*)ev_data;
 	if (mg_strcmp(hm->uri, mg_str("/ws")) == 0) {
 		mg_ws_upgrade(c, hm, NULL);
-		// TODO: Player connect
 		printf("WS: New conn\n");
 		return;
 	}
@@ -28,14 +39,23 @@ void ev_handle_http_msg(struct mg_connection* c, void* ev_data) {
 }
 
 void ev_handle_mg_msg(struct mg_connection* c, void* ev_data) {
-		// Testing: ws.send("something");
-		struct mg_ws_message* wm = (struct mg_ws_message*)ev_data;
-		printf("%s\n", wm->data.buf);
+	// Testing: ws.send("something");
+	struct mg_ws_message* wm = (struct mg_ws_message*)ev_data;
+	if (wm->data.len < 2) {
+		// TODO: Empty msg error
+		return;
+	}
+	switch (wm->data.buf[0]) {
+		case 'r':
+			ev_user_room_conn(c, &wm->data.buf[1]);
+			break;
+	}
+	printf("%s\n", wm->data.buf);
 }
 
 void ev_handle_mg_close(struct mg_connection* c, void* ev_data) {
-		//struct mg_ws_message* wm = (struct mg_ws_message*)ev_data;
-		//disconnect
+	//struct mg_ws_message* wm = (struct mg_ws_message*)ev_data;
+	//disconnect
 }
 
 void ev_handler(struct mg_connection* c, int ev, void* ev_data) {
