28242fe48dd6c0bd1f03c1d7932a3aca77c9d722
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-07-16 21:30:54 +0300

    Added connmgr.h connection manager header lib sketch

---
+++ src/connmgr.h
@@ -0,0 +1,16 @@
+// connmgr.h
+
+#ifndef connmgr_h
+#define connmgr_h
+
+#endif
+
+// connmgr.c
+
+#if defined(RRSTR_IMPLEMENTATION)
+#ifndef connmgr_c
+#define connmgr_c
+
+
+#endif /* connmgr_c */
+#endif /* CONNMGR_IMPLEMENTATION */
