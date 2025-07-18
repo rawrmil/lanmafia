ba42659cc4f13b26c626b1c403274f64690c753e
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-07-18 21:36:49 +0300

    Tests: now using one manager

--- src/tests/ut_backend.c
+++ src/tests/ut_backend.c
@@ -6,13 +6,15 @@
 #include "sds.h"
 
 struct ut_ws_conn {
+	int index;
 	sds responce;
 };
 
 void ev_handle(struct mg_connection* c, int ev, void* ev_data) {
+
 	switch (ev) {
 		case MG_EV_WS_OPEN:
-			printf("CONNECTION SUCCESSEFUL\n");
+			printf("CONNECTION %d OK\n", ((struct ut_ws_conn*)c->fn_data)->index);
 			break;
 		case MG_EV_WS_MSG:
 			//struct mg_ws_message *wm = (struct mg_ws_message*)ev_data;
@@ -34,12 +36,13 @@ void ev_handle(struct mg_connection* c, int ev, void* ev_data) {
 
 int main() {
 	printf("BACKEND TESTS:\n");
-	struct mg_mgr mgr[16];
+	struct mg_mgr mgr;
 	struct mg_connection* wsc[16];
-	struct ut_ws_conn utwsc[16];
+	struct ut_ws_conn utwsc[16] = {0};
+	mg_mgr_init(&mgr);
 	for (int i = 0; i < 16; i++) {
-		mg_mgr_init(&mgr[i]);
-		wsc[i] = mg_ws_connect(&mgr[i], "http://localhost:6969/ws", ev_handle, &utwsc[i], NULL);
+		utwsc[i].index = i;
+		wsc[i] = mg_ws_connect(&mgr, "http://localhost:6969/ws", ev_handle, &utwsc[i], NULL);
 		if (wsc[i] == NULL) {
 			printf("WS-SERVER CONNECTION ERROR\n");
 			return 1;
@@ -47,7 +50,7 @@ int main() {
 	}
 	for (;;) {
 		for (int i = 0; i < 16; i++) {
-			mg_mgr_poll(&mgr[i], 1000);
+			mg_mgr_poll(&mgr, 1000);
 		}
 	}
 	return 0;
