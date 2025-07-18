4c8d3cf7052602a166681554a12a9ae9427d348d
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-07-18 18:42:14 +0300

    Added tests setup to Makefile, let's write this bois

---
+++ src/tests/ut_backend.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+#include "mongoose.h"
+#include "sds.h"
+
+void* ws_poll() {
+	
+}
+
+int main() {
+	printf("BACKEND TESTS:\n");
+	return 0;
+}
