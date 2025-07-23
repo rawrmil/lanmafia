3a70804c3b800d7a3052f30beb45e749d4da9b56
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-07-23 15:37:05 +0300

    Added VERBOSE flag in unit tests

--- src/ut_backend.c
+++ src/ut_backend.c
@@ -6,7 +6,11 @@
 #include "mongoose.h"
 #include "sds.h"
 
-char is_connected = 0;
+#ifdef VERBOSE
+#define LOG_VERBOSE(fmt, ...) printf(fmt, ##__VA_ARGS__)
+#else
+#define LOG_VERBOSE(fmt, ...)
+#endif
 
 struct ut_ws_conn {
 	int index;
@@ -19,14 +23,19 @@ void ev_handle(struct mg_connection* c, int ev, void* ev_data) {
 	switch (ev) {
 		case MG_EV_WS_OPEN:
 			{
+				LOG_VERBOSE("WS: OPEN");
 				struct ut_ws_conn* utwsc = (struct ut_ws_conn*)c->fn_data;
+				LOG_VERBOSE("INDEX: %d\n", utwsc->index);
 				utwsc->is_connected = 1;
 			}
 			break;
 		case MG_EV_WS_MSG:
 			{
+				LOG_VERBOSE("WS: MSG");
 				struct ut_ws_conn* utwsc = (struct ut_ws_conn*)c->fn_data;
+				LOG_VERBOSE("INDEX: %d\n", utwsc->index);
 				struct mg_ws_message *wm = (struct mg_ws_message*)ev_data;
+				LOG_VERBOSE("DATA: '%*.s'\n", wm->data.len, wm->data.buf);
 				utwsc->responce = sdsnewlen(wm->data.buf, wm->data.len);
 				utwsc->got_responce = 1;
 			}
@@ -61,19 +70,19 @@ void ut_expect(struct mg_connection* c, char* res) {
 }
 
 int main() {
-	printf("BACKEND TESTS:\n");
+	printf("BACKEND UNIT-TESTS:\n");
+	LOG_VERBOSE("TEST VERBOSE\n");
+	printf("Initializing WS connections...\n");
 	struct mg_mgr mgr;
 	struct mg_connection* c[16];
 	struct ut_ws_conn utwsc[16] = {0};
 	mg_mgr_init(&mgr);
 	for (int i = 0; i < 16; i++) {
 		utwsc[i].index = i;
+		printf("Connection %d... \n");
 		c[i] = mg_ws_connect(&mgr, "http://localhost:6969/ws", ev_handle, &utwsc[i], NULL);
-		if (c[i] == NULL) {
-			printf("WS-SERVER CONNECTION ERROR\n");
-			return 1;
-		}
 	}
+	printf("Begin testing...\n");
 	ut_connect(c[0]);
 	ut_send(c[0], "c_open|Coolname666");
 	ut_expect(c[0], "c_open_ok");
