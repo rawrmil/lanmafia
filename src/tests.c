d464cabf1fbc901e4e5a85e72c755118bb64b9b7
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-07-24 19:43:45 +0300

    WIP: Fixing tests

--- src/tests.c
+++ src/tests.c
@@ -91,44 +91,27 @@ void ut_expect(struct mg_connection* c, char* res) {
 // T E S T S
 
 char test_conn_open(struct mg_connection* c[]) {
+	ut_connect(c[0]);
 	ut_send(c[0], "c_open|Coolname666");
 	ut_expect(c[0], "c_open_ok");
 	ut_expect(c[0], "c_users|Coolname666");
-	return 1;
 }
-
-#define UNIT_TEST(server_thread_, c_, name_, testfunc_)                  \
-	do {                                                                   \
-		ut_success = 0;                                                      \
-		pthread_join(server_thread, NULL);                                   \
-		printf("UNIT-TEST: %s: %s", name_, testfunc_(c_) ? "✔️OK" : "❌ERR"); \
-		pthread_cancel(server_thread);                                       \
-	} while (0);
 
 // M A I N
 
 void a_main(int argc, char* argv[]); // From mafia.c
 
 void* server_thread_func(void* arg) {
-	char* argv[] = { "mafia", "--port", "7998" };
-	a_main(3, argv);
+	char* argv[] = { "mafia", "--port", "7998", DEBUG ? "-D" : "-F"};
+	a_main(4, argv);
 }
 
-int main() {
-	printf("BACKEND UNIT-TESTS:\n");
-
-	LOG_DEBUG("Initializing server thread...\n");
-
+void unit_test(char* name, char (*testfunc)(struct mg_connection* c[])) {
 	pthread_t server_thread;
 	if (pthread_create(&server_thread, NULL, server_thread_func, NULL)) {
 		printf("FATAL: Cannot create a thread");
-		return 1;
+		exit(1);
 	}
-
-	sleep(1);
-
-	LOG_DEBUG("Initializing WS connections...\n");
-
 	struct mg_mgr mgr;
 	struct mg_connection* c[16];
 	struct ut_ws_conn utwsc[16] = {0};
@@ -138,12 +121,14 @@ int main() {
 		LOG_DEBUG("Connection %d... \n", i);
 		c[i] = mg_ws_connect(&mgr, "http://localhost:7998/ws", ev_handle, &utwsc[i], NULL);
 	}
+	sleep(2);
+	ut_success = 0;
+	printf("UNIT-TEST: %s: %s", name, testfunc(c) ? "✔️OK" : "❌ERR");
+	pthread_cancel(server_thread);
+}
 
-	LOG_DEBUG("Begin testing...\n");
-
-	ut_connect(c[0]);
-
-	UNIT_TEST(server_thread, c, "CONNECTION OPEN", test_conn_open);
-
+int main() {
+	printf("BACKEND UNIT-TESTS:\n");
+	unit_test("CONNECTION OPEN", test_conn_open);
 	return 0;
 }
